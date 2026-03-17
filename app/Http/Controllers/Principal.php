bea89b07c8375cdc05f7da7b0bb5c0ec979e8be4
Gabriel-Lauriano <zyogabriel02@gmail.com>
2026-03-17 14:47:22 -0300

    manipulacao

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -10,7 +10,22 @@ class Principal extends Controller
         echo 'Página Principal';
     }
 
+    function contato(string $nome){
+        echo $nome;
+    }
+
+    function contatoNomeCompleto(string $nome, string $sobrenome){
+        echo "O contato é $nome $sobrenome";
+    }
     
+    function contatoMensagem(string $nome, string $sobrenome, string $mensagem){
+        echo "O contato $nome $sobrenome enviou uma mensagem:";
+        echo "<h1>$mensagem</h1>";
+    }
+    function contatoTelefone(string $nome, string $sobrenome, string $mensagem, string $telefone, string $email = 'E-mail não informado'){
+        echo "O contato $nome $sobrenome enviou uma mensagem:";
+        echo "<h1>$mensagem</h1>";
+        echo "Telenofe; $telefone <br>";
+        echo "E-mail: $email";
+    }
 }
-
-
